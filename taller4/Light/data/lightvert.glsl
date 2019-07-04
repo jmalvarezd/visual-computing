98b6912a36c4acbe1d7d821087c83d65f1be17f3
Nicolás Campuzano <nicolas.campuzano@tulpep.com>
2019-07-04 12:28:49 -0500

    Light project

---
+++ taller4/Light/data/lightvert.glsl
@@ -0,0 +1,23 @@
+uniform mat4 modelview;
+uniform mat4 transform;
+uniform mat3 normalMatrix;
+uniform vec4 lightPosition;
+
+uniform vec3 lightDiffuse[8];
+
+attribute vec4 position;
+attribute vec4 color;
+attribute vec3 normal;
+
+varying vec4 vertColor;
+
+void main() {
+  gl_Position = transform * position;    
+  vec3 ecPosition = vec3(modelview * position);  
+  vec3 ecNormal = normalize(normalMatrix * normal);
+  vec3 lightDirection = normalize(lightPosition.xyz - ecPosition);
+  vec3 cameraDirection = normalize(0 - ecPosition);
+  vec3 lightDirectionReflected = reflect(-lightDirection, ecNormal);
+  float intensity = max(0.0, dot(lightDirectionReflected, cameraDirection));
+  vertColor = vec4(intensity*lightDiffuse[0], 1) * color;             
+}
